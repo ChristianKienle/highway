881bf143453ce8921b2de266dab3290ae0b11a85
Christian Kienle <christian.kienle@sap.com>
2017-10-24 19:16:42 +0200

    fix

--- Sources/HWKit/Command Helper/SelfUpdater.swift
+++ Sources/HWKit/Command Helper/SelfUpdater.swift
@@ -21,9 +21,15 @@ public final class SelfUpdater {
     public let git: GitTool
     
     public func update() throws {
+        // Update the local highway working copy
+        try git.pull(at: homeBundle.localCloneUrl)
+        
+        // Then get a temp. copy of highway...
         let tempUrl = try self.homeBundle.fileSystem.uniqueTemporaryDirectoryUrl()
         let cloneOptions = Git.CloneOptions(remoteUrl: homeBundle.localCloneUrl.path, localPath: tempUrl, performMirror: false)
         try git.clone(with: cloneOptions)
+        
+        // ... and execute the bootstrap script
         let bootstrapScriptUrl = tempUrl.appending("scripts").appending("bootstrap.sh")
         let args = Arguments(["-c", "(/bin/sleep 2; \(bootstrapScriptUrl.path))&"])
         let bash = try Task(commandName: "bash", arguments: args, currentDirectoryUrl: tempUrl, provider: context.executableProvider)

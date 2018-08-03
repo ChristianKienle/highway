c122be0df9cb0aa7e485bbcfb518b2203256ebc9
Stijn Willems <stijn.willems@dooz.be>
2018-08-03 18:04:02 +0200

    ZFileiOS working

---
+++ ZFileiOS/ZFileiOS.h
@@ -0,0 +1,18 @@
+//
+//  ZFileiOS.h
+//  ZFileiOS
+//
+//  Created by Stijn on 03/08/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZFileiOS.
+FOUNDATION_EXPORT double ZFileiOSVersionNumber;
+
+//! Project version string for ZFileiOS.
+FOUNDATION_EXPORT const unsigned char ZFileiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFileiOS/PublicHeader.h>
+
+
